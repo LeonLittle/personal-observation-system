02f0591bf810a5ab52cc55d4ffcbe85e07ed6d04
LeonLittle <1332283626@proton.me>
2026-05-31 18:28:19 +0800

    Update observation models and views

--- observations/models.py
+++ observations/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+#从django数据模块里拿models功能来用
 from datetime import date
+#datetime=python自带时间管理工具箱
+#date是datetime时间管理工具箱里面的纯日期
 
 class DailyRecord(models.Model):
     """
@@ -14,32 +17,37 @@ class DailyRecord(models.Model):
     """
 
     date = models.DateField(
-        default=date.today,
-        unique=True,
+        default=date.today, #default=默认值 date.today=今天的日期 default=date.today=如果没有指定日期,就默认为今天的日期
+        unique=True,    #unique=唯一 unique+True  =  唯一+开启 =  这个日期不能重复
         verbose_name="日期"
     )
+    #models.DateField=纯日期字段
 
     mood =models.CharField(
-        max_length=20,
-        blank=True,
+        max_length=20,  #这个字段最长为20
+        blank=True, 
         verbose_name="心情"
     )
+    #models.CharField=短文本字段
 
     energy = models.IntegerField(
-        null=True,
-        blank=True,
-        verbose_name="精力分"
+        null=True,  #null=数据库 null+True=允许数据库为空,用户非必须填写精力值
+        blank=True, #blank=表单 允许为空
+        verbose_name="精力值"
     )
+    #models.IntegerField=整数数字字段
 
     summary = models.TextField(
-        blank=True,
+        blank=True,  #blank=表单 blank+True=允许字段'表单'为空,用户非必须填写
         verbose_name = "今日总结"
     )
+    #长文本不限制长度
 
     updated_at=models.DateTimeField(
-        auto_now_add=True,
+        auto_now_add=True, #auto_now_add=只在创建时记录一次时间 anto_now_add+True=默认在创建时生成一次时间.
         verbose_name="创建时间"
     )
+    #DateTimeField=日期+时间字段   django---定义数据库字段类型
 
     def __str__(self):
         """
@@ -53,26 +61,38 @@ class Task(models.Model):
     Task 代表一条每日任务
 
     现在是第一版，所以字段先保持简单：
-    1. title:任务标题
+    1. title:任务内容
     2. is_done:是否完成
     3. created_at:创建时间
+    4. models.CharField是 django里的函数 models.CharField后面的参数都是django里的参数
+    5.CharField短文本字段
+    6.BooleanField布尔字段
+    7.DateTimeField日期+时间字段
+    8.verbose_name命名参数
+    9.default默认参数
+    10.auto_now_add记录第一次创建时间
+    11.str 对象默认显示名称
     """
 
     title = models.CharField(
         max_length=100,verbose_name="任务标题"
     )
+    #CharField=短文本字段 max_length=100 最大字符100  verbose_name是django里面的命名参数
+
 
     is_done = models.BooleanField(
         default=False,verbose_name="是否完成"
     )
+    #BooleanField布尔字段 只有True/False这两种状态  default=False默认是空,手动完成
 
     created_at = models.DateTimeField(
         auto_now_add=True,verbose_name="创建时间"
     )
+    #DateTimeField日期+时间字段 auto_now_add=True 自动生成第一次创建时间 verbose_name=django里的命名参数
 
     def __str__(self):
         """
-        当Django后台或调试时显示这个对象,
-        直接显示任务标题，方便我们识别。
+        当Django后台超级管理网站或调试时显示这个Task对象,
+        直接显示title任务标题,方便我们识别,不然django默认显示Task object (1)。
         """
         return self.title
--- observations/views.py
+++ observations/views.py
@@ -1,11 +1,12 @@
-
 from django.shortcuts import render,redirect,get_object_or_404
 from datetime import datetime
-
 from .models import Task,DailyRecord
+#1. render作用:把数据交给HTML页面,然后生成网页返回给浏览器 意思:生成网页
+#2. redirect作用:跳转到另一个页面  意思:保存完任务后,重新回到首页 
+#3. get_object_or_404作用:代码安全维护  意思:去数据库找某条数据;找不到就显示404错误页
+#4. from datetime import datetime是在python里的datetime时间管理模块 导入datetime日期+时间工具
+#5. shortcuts是django快捷工具箱
 
-
-# 定义home视图函数，处理首页的HTTP请求
 def home(request):
     """ 首页视图函数。
 
@@ -16,53 +17,87 @@ def home(request):
     4. 处理今日状态保存
     5. 从数据库读取所有任务
     6. 把数据交给 home.html 页面显示
+    7. request获取浏览器所有信息
+    8. get_or_create查询or创建 是否刚创建True/Flash,所以必须要有两个变量来接收
     """
     
     today = datetime.now()
+    #命名了一个today变量来获取今日日期+时间
 
-    #获取或创建今天的DailyRecord
-    #如果今天已经有记录,就拿出来
-    #如果今天还没有记录,就自动创建一条
     today_record,created = DailyRecord.objects.get_or_create(
         date=today.date()
     )
+    #today_record接收DailyRecord里的符合today.date日期的数据 
+    #get_or_create如果有数据直接给today同时返回一个flash给created
+    #get_or_create如果没有数据就新建一个符合today.date日期的数据同时返回一个True给created
 
-    #如果用户是通过表单提交任务,也就是点击了"添加任务"按钮
+
     if request.method == "POST":
-        #从网页表里取出name=from_type 输入的内容
+        #method 浏览器的请求方式GET/POST
+        #判断用户是否提交数据
+
         form_type = request.POST.get("form_type")
+        #获取浏览器里提交的("form_type")数据给到变量form_type
 
         if form_type == "task":
-            #从网页表里取出name="title"输入的内容
-            title = request.POST.get("title")
+            #判断变量form_type里的数据是不是task
 
-        #如果用户真的输入了内容,才创建任务 
+            title = request.POST.get("title")
+            #获取浏览器提交的("title")数据给到变量title
+
             if title:
                 Task.objects.create(title=title)
-
+            #如果变量title里有数据 create就在Task类里添加一条数据
+            #并把变量里的title数据 保存到Task类里的title字段
+            
+            #刷新home页面
             return redirect("home")
 
         if form_type =="daily_record":
+            #判断form_type变量里的数据是不是daily_record
+
             today_record.mood = request.POST.get("mood","")
+            #获取浏览器里提交的mood内容,保存到today_record对象里的mood字段,如果没有就为空字符串
+
             energy = request.POST.get("energy")
+            #获取浏览器提交的("energy")字符串数据先给到变量energy
+
             today_record.summary = request.POST.get("summary","")
+            #获取浏览器里提交的summary内容,保存到today_record对象里的sunmmary字段,如果没有就为空字符串
 
         if energy:
+            #判断用户有没填写energy数据
+
             today_record.energy = int(energy)
+            #把字符串energy转换成整型,保存到today_record数据表里的energy字段
         
             today_record.save()
+            #.save()保存today_record数据
 
             return redirect("home")
+            #刷新home页面
 
     tasks = Task.objects.all().order_by("-created_at")
+    #created_at是Task里创建时间字段
+    #("-created_at")-倒序
+    #.order_by() 按某个字段排序
 
     total_count = tasks.count()
+    #.count()统计数量
+    #.count()统计tasks里有多少数量 保存到total_count里
+
     done_count = tasks.filter(is_done=True).count()
+    #.filter() 筛选
+    #tasks.filter(is_done=True)筛选(is_done=True)的数据
+    #筛选完后.count()统计数量
+    #统计有多少已完成的任务 保存到done_count里
 
-
-    # 创建context字典，用于存储传递给模板的数据
     context = {
-
+        #左边是html里使用的名字
+        #Python里的真实变量
+        #.strftime()把日期时间05/31格式化成指定名字05月/31日
+        #today.weekday()
+        #.weekday()计算星期几,并返回对应编号
         "title":"个人观察助手",
         "date_text" : today.strftime("%m月%d日"),
         "weekday_text":["星期一","星期二","星期三","星期四","星期五","星期六","星期日"][today.weekday()],
@@ -72,13 +107,12 @@ def home(request):
         "today_record":today_record,
     }
 
-    # 返回渲染后的home.html模板，并将context数据传递给模板使用
     return render(request,"observations/home.html",context)
-
+    #把context的数据交给"observations/home.html" render生成到最终页面
 
 def toggle_task(request,task_id):
     """
-    切换任务完成状态
+    toggle切换 任务完成状态
 
     作用：
     1. 根据 task_id 找到对应任务
@@ -88,19 +122,18 @@ def toggle_task(request,task_id):
     5. 回到首页
     """
 
-    #根据任务id从数据库里找到这条任务
     task = get_object_or_404(Task,id=task_id)
+    #不知道什么意思
 
-    #把当前状态反过来
-    #原来是False,就变成True
-    #原来是True,就变成False
     task.is_done = not task.is_done
+    #访问task对象里的is_done字段 not 取反 代表的是True/Flash
+    #然后保存到左边的task.is_done里
 
-    #保存修改
     task.save()
+    #保存task的数据
 
-    #回到首页
     return redirect("home")
+    #刷新home页面
 
 def delete_task(request,task_id):
     """
@@ -112,13 +145,12 @@ def delete_task(request,task_id):
     3,回到首页
     """
 
-    #根据任务id从出数据库里找到这条任务
     task = get_object_or_404(Task,id=task_id)
+    #暂时不去理解
 
-    #删除这条任务
     task.delete()
+    #task执行删除
     
-    #删除后回到首页
     return redirect("home")
-
+    #刷新home页面
 
