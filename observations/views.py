16738ae8b0ef23e41f832c16b1ac14ded78eeefd
LeonLittle <1332283626@proton.me>
2026-06-02 17:04:52 +0800

    Show task planned and completed time

--- observations/views.py
+++ observations/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect,get_object_or_404
-from datetime import datetime
+from datetime import timezone
 from .models import Task,DailyRecord
 #1. render作用:把数据交给HTML页面,然后生成网页返回给浏览器 意思:生成网页
 #2. redirect作用:跳转到另一个页面  意思:保存完任务后,重新回到首页 
@@ -303,7 +303,7 @@ def toggle_task(request, task_id):
     # 如果当前任务还没完成
     if not task.is_done:
         task.is_done = True
-        task.completed_at = datetime.now()
+        task.completed_at = timezone.now()
     else:
         task.is_done = False
         task.completed_at = None
